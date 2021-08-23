7dccf88797ae8704c55fc0414956ce0e9bd66ad1
fxhmi <50392574+fxhmi@users.noreply.github.com>
2021-08-24 02:12:03 +0800

    updated UI

---
+++ lib/animation.dart
@@ -0,0 +1,83 @@
+
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+final signupPurpleGradient = LinearGradient(colors: [
+  Color.fromRGBO(145, 131, 222, 1),
+  Color.fromRGBO(160, 148, 227, 1),
+]);
+final signupDarkPurple = Color.fromRGBO(82, 67, 154, 1);
+
+final loginPurpleGradient = LinearGradient(colors: [
+  Color.fromRGBO(229, 178, 202, 1),
+  Color.fromRGBO(205, 130, 222, 1),
+]);
+final loginDarkPurple = Color.fromRGBO(120, 37, 139, 1);
+
+
+class AnimationTest extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final topPadding = MediaQuery.of(context).padding.top;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromRGBO(145, 131, 222, 1),
+          Color.fromRGBO(160, 148, 227, 1),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: topPadding),
+              SizedBox(height: 10),
+              AnimatedImage(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedImage extends StatefulWidget {
+  @override
+  _AnimatedImageState createState() => _AnimatedImageState();
+}
+
+class _AnimatedImageState extends State<AnimatedImage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _animation = Tween<Offset>(
+    begin: Offset.zero,
+    end: Offset(0, 0.08),
+  ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset('assets/images/clouds.png'),
+        SlideTransition(
+          position: _animation,
+          child: Image.asset('assets/images/rocket_person.png'),
+        ),
+      ],
+    );
+  }
+}
